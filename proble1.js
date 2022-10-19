83c9b49d278ab13c96938d079011a44ca2335ede
Dhirajrrajput123 <dhirajrajpp@gmail.com>
2022-10-19 18:15:06 +0530

    currect it

--- proble1.js
+++ proble1.js
@@ -1,1 +1,17 @@
+//find the prime number
 let num=19;
+ let prime=true;
+for(let i=2;i<=num/2;i++){
+ 
+if(num%i==0){
+num=false;
+
+}
+
+if(prime==true){
+
+console.log(num," is prime number");
+}
+else{
+consoel.log(num," is not prime number");
+}
--- problem2.js
+++ problem2.js
@@ -1,1 +1,16 @@
 let str="maggam";
+
+let str2="";
+
+for(let i=str.length-1;i>0;i++){
+
+    str2+=str[i];
+}
+
+if(str==str1){
+consoele.log("palindrom");
+}
+else{
+console.log("not palindrom");
+}
+
